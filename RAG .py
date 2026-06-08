32217d0f610c1d1ae1aa0220b41573e0f16a8941
faizan6xyz <232054644+faizan6xyz@users.noreply.github.com>
2026-06-08 06:15:09 +0530

    testing failed very badly

--- RAG .py
+++ RAG .py
@@ -13,14 +13,31 @@ for file in os.listdir(folder_path):
             paragraph = paragraph.strip()
             if paragraph:
                 all_chunks.append(paragraph)
-print("Total chunks:", len(all_chunks))
+print(f"Total chunks: {len(all_chunks)}")
 model = SentenceTransformer("BAAI/bge-base-en-v1.5")
 embeddings = model.encode(all_chunks,convert_to_numpy=True)
 print("Embedding shape:", embeddings.shape)
-dimension = embeddings.shape[1]
-index = faiss.IndexFlatL2(dimension)
-index.add(np.array(embeddings, dtype=np.float32))
-print("Vectors stored:", index.ntotal)
+faiss.normalize_L2(embeddings) # give (10 number of the text chunk , 786 size of each embedding vector )
+dimension = embeddings.shape[1] # get the value of dimension
+index = faiss.IndexFlatIP(dimension) # this Create an empty FAISS database.
+index.add(embeddings.astype(np.float32))# Store all embeddings inside that database for fast similarity search.
+print("Vectors stored:", index.ntotal) 
 faiss.write_index(index, "rag_index.faiss") # Stores the vector embeddings
-np.save("chunks.npy", np.array(all_chunks)) # Stores the original paragraph text corresponding to each vector 
+np.save("chunks.npy", np.array(all_chunks)) # Stores the original paragraph text corresponding to each vector
 print("FAISS index saved successfully.")
+# Load FAISS index , index = faiss.read_index("rag_index.faiss") 
+# Load text chunks , all_chunks = np.load("chunks.npy", allow_pickle=True)
+while True:
+    query = input("\nEnter your query (type 'exit' to quit): ")
+    if query.lower() == "exit":
+        break
+    query_embedding = model.encode(query,convert_to_numpy=True)
+    query_embedding = np.array([query_embedding], dtype=np.float32)
+    faiss.normalize_L2(query_embedding)
+    scores, indices = index.search(query_embedding, k=3)
+    print("\nTop matching chunks:\n")
+    for rank, idx in enumerate(indices[0]):
+        print(f"Rank {rank + 1}")
+        print(f"Similarity Score: {scores[0][rank]:.4f}")
+        print(all_chunks[idx])
+        print("-" * 60)
